0abf31b6df5f1ec0a88e5479b3d2a0c42f42a2ef
Iván Landaverde <2502882019@mail.utec.edu.sv>
2024-07-29 22:48:09 -0600

    Entendiendo relaciones de uno a uno

---
+++ database/migrations/2024_07_30_040542_create_phones_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');               
+            // $table->unsignedInteger('user_id');     // Para los valores que sean lalves foraneas se crean del tipo unsignedBigInteger
+            // $table->foreign('user_id')              // Luego de eso se establece la relacion
+            //     ->references('id')                  // Campo de la otra tabla hacia el cual se hace referencia
+            //     ->on('users')                       // Tabla a la que se hace referencia
+            //     ->onDelete('cascade')               // Al realizar la accion de eliminar, eliminar todos los registros relacionados a este mismo
+            //     ->onUpdate('cascade');              // Al realizar la accion de actualizar, actualizar todos los registros relacionados a este mismo
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Schema::dropColumns('user_id');
+        Schema::dropIfExists('phones');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Models\Phone;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Ruta de prueba para ingresar registros
+Route::get('/pruebas', function () {
+    
+    $user = User::create([
+        'name' => 'Ivan Mendoza',
+        'email' => 'ivan@gmail.com',
+        'password' => bcrypt('12345678'),
+    ]);
+
+    return "Usuario creado {$user}";
+});
+
+Route::get('phone', function () {
+
+    $phone = Phone::create([
+        'number' => '2548-9615',
+        'user_id' => 1,
+    ]);
+
+    return "Registro ingresado {$phone}";
 });
